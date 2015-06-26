77a19929879a99a7d772aa7dca520937263abed9
ototadana <ototadana@gmail.com>
2015-06-27 08:29:17 +0900

    Add a workaround for SonarQube error
    See: http://stackoverflow.com/questions/30459260/jacoco-sonarqube-incompatible-version-1007

--- template/code/example-java/build.sh
+++ template/code/example-java/build.sh
@@ -3,4 +3,4 @@ set -eu
 
 BUILD_OPTS="-Dmaven.test.failure.ignore=true"
 
-mvn -B ${BUILD_OPTS} clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar
+mvn -B ${BUILD_OPTS} clean org.jacoco:jacoco-maven-plugin:0.7.4.201502262128:prepare-agent install sonar:sonar
