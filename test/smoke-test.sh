f3b05e81b4f7483054dfe0a9076ceb892e975d06
ototadana <ototadana@gmail.com>
2015-08-15 13:31:41 +0900

    テスト実行方法がうまくいかない試行錯誤中

--- test/smoke-test.sh
+++ test/smoke-test.sh
@@ -12,14 +12,20 @@ cd ${BASE_DIR}/temp/pocci/bin/js
 sudo rm -fr ${BASE_DIR}/temp/pocci/backup/*
 ${BASE_DIR}/temp/pocci/bin/backup
 
-../oneoff iojs grunt mochaTest:jenkinsNodeJSBuild
-../oneoff iojs grunt basic mochaTest:login
-../oneoff iojs grunt mochaTest:jenkinsJavaBuild
+../oneoff iojs grunt basic
+../oneoff iojs grunt prepare mochaTest:login
 ../oneoff iojs grunt prepare mochaTest:gitlab
+sleep 30
+../oneoff iojs grunt prepare mochaTest:jenkinsNodeJSBuild
+sleep 120
+../oneoff iojs grunt prepare mochaTest:jenkinsJavaBuild
 
 echo 'y' | ${BASE_DIR}/temp/pocci/bin/restore ${BASE_DIR}/temp/pocci/backup/*
 
-../oneoff iojs grunt mochaTest:jenkinsNodeJSBuild
-../oneoff iojs grunt basic mochaTest:login
-../oneoff iojs grunt mochaTest:jenkinsJavaBuild
+../oneoff iojs grunt basic
+../oneoff iojs grunt prepare mochaTest:login
 ../oneoff iojs grunt prepare mochaTest:gitlab
+sleep 30
+../oneoff iojs grunt prepare mochaTest:jenkinsNodeJSBuild
+sleep 120
+../oneoff iojs grunt prepare mochaTest:jenkinsJavaBuild
