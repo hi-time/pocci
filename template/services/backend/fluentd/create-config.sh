18f04847148a0a353287bee453e4a47c20bcc415
ototadana <ototadana@gmail.com>
2016-01-06 22:20:59 +0900

    fluentdコンテナのログが正常に出力されていなかった問題を修正

--- template/services/backend/fluentd/create-config.sh
+++ template/services/backend/fluentd/create-config.sh
@@ -1,11 +1,18 @@
 
 LOG_CONFIG_FILE=${CONFIG_DIR}/fluent.conf
 
+if [ -f ${LOG_CONFIG_FILE} ]; then
+    rm ${LOG_CONFIG_FILE}
+fi
+
 for i in ${INTERNAL_SERVICES}; do
     if [ -f ${TEMPLATE_DIR}/services/core/$i/fluent.conf.template ]; then
         source ${TEMPLATE_DIR}/services/core/$i/fluent.conf.template >>${LOG_CONFIG_FILE}
     fi
 done
+
+cat ${TEMP_CONFIG_DIR}/fluent.conf >>${LOG_CONFIG_FILE}
+
 
 if [ ! -d ${POCCI_LOG_DIR} ]; then
     mkdir ${POCCI_LOG_DIR}
