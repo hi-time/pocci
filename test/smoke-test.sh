84c4fbca25dfc7ddc22e33546f18213772d55beb
ototadana <ototadana@gmail.com>
2016-04-13 00:26:05 +0900

    test-private を途中から再実行できるようにする

--- test/smoke-test.sh
+++ test/smoke-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ex
+set -e
 
 POCCI_REPO=$1
 
@@ -7,79 +7,123 @@ BASE_DIR=$(cd $(dirname $0); pwd)
 LOG_FILE=${BASE_DIR}/temp/test.log
 
 trap "date |tee -a ${LOG_FILE}" EXIT
-${BASE_DIR}/do-instructions-in-readme.sh ${POCCI_REPO}
 
-cd ${BASE_DIR}/temp/pocci/bin/js
+step1() {
+    ${BASE_DIR}/do-instructions-in-readme.sh ${POCCI_REPO}
 
-sleep 30
-echo $(date): BACKUP default >> ${LOG_FILE}
-sudo rm -fr ${BASE_DIR}/temp/pocci/backup/*
-${BASE_DIR}/temp/pocci/bin/backup
-sudo mv ${BASE_DIR}/temp/pocci/backup ${BASE_DIR}/temp/pocci/backup_default
+    sleep 30
+    echo $(date): BACKUP default >> ${LOG_FILE}
+    sudo rm -fr ${BASE_DIR}/temp/pocci/backup/*
+    ${BASE_DIR}/temp/pocci/bin/backup
+    sudo mv ${BASE_DIR}/temp/pocci/backup ${BASE_DIR}/temp/pocci/backup_default
+}
 
-echo $(date): TEST_1 default >> ${LOG_FILE}
-../oneoff nodejs grunt basic
-../oneoff nodejs grunt prepare mochaTest:loginDefault
-../oneoff nodejs grunt prepare mochaTest:defaultSetup
-${BASE_DIR}/build-test.sh
+step2() {
+    echo $(date): TEST_1 default >> ${LOG_FILE}
+    cd ${BASE_DIR}/temp/pocci/bin/js
+    ../oneoff nodejs grunt basic
+    ../oneoff nodejs grunt prepare mochaTest:loginDefault
+    ../oneoff nodejs grunt prepare mochaTest:defaultSetup
+    ${BASE_DIR}/build-test.sh
+}
 
-echo $(date): SETUP jenkins >> ${LOG_FILE}
-echo 'y' | ${BASE_DIR}/temp/pocci/bin/create-config jenkins
-${BASE_DIR}/temp/pocci/bin/up-service
+step3() {
+    echo $(date): SETUP jenkins >> ${LOG_FILE}
+    echo 'y' | ${BASE_DIR}/temp/pocci/bin/create-config jenkins
+    ${BASE_DIR}/temp/pocci/bin/up-service
 
-sleep 30
-echo $(date): BACKUP jenkins >> ${LOG_FILE}
-${BASE_DIR}/temp/pocci/bin/backup
-sudo mv ${BASE_DIR}/temp/pocci/backup ${BASE_DIR}/temp/pocci/backup_jenkins
+    sleep 30
+    echo $(date): BACKUP jenkins >> ${LOG_FILE}
+    ${BASE_DIR}/temp/pocci/bin/backup
+    sudo mv ${BASE_DIR}/temp/pocci/backup ${BASE_DIR}/temp/pocci/backup_jenkins
+}
 
-echo $(date): TEST_1 jenkins >> ${LOG_FILE}
-../oneoff nodejs grunt basic
-../oneoff nodejs grunt prepare mochaTest:loginJenkins
-../oneoff nodejs grunt prepare mochaTest:jenkinsSetup
-../oneoff nodejs grunt prepare mochaTest:gitlab
-${BASE_DIR}/build-test.sh
+step4() {
+    echo $(date): TEST_1 jenkins >> ${LOG_FILE}
+    cd ${BASE_DIR}/temp/pocci/bin/js
+    ../oneoff nodejs grunt basic
+    ../oneoff nodejs grunt prepare mochaTest:loginJenkins
+    ../oneoff nodejs grunt prepare mochaTest:jenkinsSetup
+    ../oneoff nodejs grunt prepare mochaTest:gitlab
+    ${BASE_DIR}/build-test.sh
+}
 
-echo $(date): SETUP redmine >> ${LOG_FILE}
-echo 'y' | ${BASE_DIR}/temp/pocci/bin/create-config redmine
-${BASE_DIR}/temp/pocci/bin/up-service
+step5() {
+    echo $(date): SETUP redmine >> ${LOG_FILE}
+    echo 'y' | ${BASE_DIR}/temp/pocci/bin/create-config redmine
+    ${BASE_DIR}/temp/pocci/bin/up-service
 
-sleep 30
-echo $(date): BACKUP redmine >> ${LOG_FILE}
-${BASE_DIR}/temp/pocci/bin/backup
-sudo mv ${BASE_DIR}/temp/pocci/backup ${BASE_DIR}/temp/pocci/backup_redmine
+    sleep 30
+    echo $(date): BACKUP redmine >> ${LOG_FILE}
+    ${BASE_DIR}/temp/pocci/bin/backup
+    sudo mv ${BASE_DIR}/temp/pocci/backup ${BASE_DIR}/temp/pocci/backup_redmine
+}
 
-echo $(date): TEST_1 redmine >> ${LOG_FILE}
-../oneoff nodejs grunt basic
-../oneoff nodejs grunt prepare mochaTest:loginRedmine
-../oneoff nodejs grunt prepare mochaTest:redmineSetup
-${BASE_DIR}/build-test.sh
+step6() {
+    echo $(date): TEST_1 redmine >> ${LOG_FILE}
+    cd ${BASE_DIR}/temp/pocci/bin/js
+    ../oneoff nodejs grunt basic
+    ../oneoff nodejs grunt prepare mochaTest:loginRedmine
+    ../oneoff nodejs grunt prepare mochaTest:redmineSetup
+    ${BASE_DIR}/build-test.sh
+}
 
-echo $(date): RESTORE default >> ${LOG_FILE}
-echo 'y' | ${BASE_DIR}/temp/pocci/bin/restore ${BASE_DIR}/temp/pocci/backup_default/*
+step7() {
+    echo $(date): RESTORE default >> ${LOG_FILE}
+    echo 'y' | ${BASE_DIR}/temp/pocci/bin/restore ${BASE_DIR}/temp/pocci/backup_default/*
+}
 
-echo $(date): TEST_2 default >> ${LOG_FILE}
-../oneoff nodejs grunt basic
-../oneoff nodejs grunt prepare mochaTest:loginDefault
-../oneoff nodejs grunt prepare mochaTest:defaultSetup
-${BASE_DIR}/build-test.sh
+step8() {
+    echo $(date): TEST_2 default >> ${LOG_FILE}
+    cd ${BASE_DIR}/temp/pocci/bin/js
+    ../oneoff nodejs grunt basic
+    ../oneoff nodejs grunt prepare mochaTest:loginDefault
+    ../oneoff nodejs grunt prepare mochaTest:defaultSetup
+    ${BASE_DIR}/build-test.sh
+}
 
+step9() {
+    echo $(date): RESTORE redmine >> ${LOG_FILE}
+    echo 'y' | ${BASE_DIR}/temp/pocci/bin/restore ${BASE_DIR}/temp/pocci/backup_redmine/*
+}
 
-echo $(date): RESTORE redmine >> ${LOG_FILE}
-echo 'y' | ${BASE_DIR}/temp/pocci/bin/restore ${BASE_DIR}/temp/pocci/backup_redmine/*
+step10() {
+    echo $(date): TEST_2 redmine >> ${LOG_FILE}
+    cd ${BASE_DIR}/temp/pocci/bin/js
+    ../oneoff nodejs grunt basic
+    ../oneoff nodejs grunt prepare mochaTest:loginRedmine
+    ../oneoff nodejs grunt prepare mochaTest:redmineSetup
+    ${BASE_DIR}/build-test.sh
+}
 
-echo $(date): TEST_2 redmine >> ${LOG_FILE}
-../oneoff nodejs grunt basic
-../oneoff nodejs grunt prepare mochaTest:loginRedmine
-../oneoff nodejs grunt prepare mochaTest:redmineSetup
-${BASE_DIR}/build-test.sh
+step11() {
+    echo $(date): RESTORE jenkins >> ${LOG_FILE}
+    echo 'y' | ${BASE_DIR}/temp/pocci/bin/restore ${BASE_DIR}/temp/pocci/backup_jenkins/*
+}
 
+step12() {
+    echo $(date): TEST_2 jenkins >> ${LOG_FILE}
+    cd ${BASE_DIR}/temp/pocci/bin/js
+    ../oneoff nodejs grunt basic
+    ../oneoff nodejs grunt prepare mochaTest:loginJenkins
+    ../oneoff nodejs grunt prepare mochaTest:jenkinsSetup
+    ../oneoff nodejs grunt prepare mochaTest:gitlab
+    ${BASE_DIR}/build-test.sh
+}
 
-echo $(date): RESTORE jenkins >> ${LOG_FILE}
-echo 'y' | ${BASE_DIR}/temp/pocci/bin/restore ${BASE_DIR}/temp/pocci/backup_jenkins/*
+step13() {
+    echo "Done" | tee -a ${LOG_FILE}
+}
 
-echo $(date): TEST_2 jenkins >> ${LOG_FILE}
-../oneoff nodejs grunt basic
-../oneoff nodejs grunt prepare mochaTest:loginJenkins
-../oneoff nodejs grunt prepare mochaTest:jenkinsSetup
-../oneoff nodejs grunt prepare mochaTest:gitlab
-${BASE_DIR}/build-test.sh
+START=1
+if [ -n "$1" ]; then
+    START=$2
+fi
+
+for i in `seq ${START} 13`; do
+    if [ -f ${LOG_FILE} ]; then
+        echo "step$i" | tee -a ${LOG_FILE}
+    fi
+  step$i
+done
+
--- test/test-private.sh
+++ test/test-private.sh
@@ -3,5 +3,7 @@ set -ex
 
 BASE_DIR=$(cd $(dirname $0); pwd)
 
-${BASE_DIR}/clean-containers.sh
-${BASE_DIR}/smoke-test.sh ${BASE_DIR}/../.git
+if [ -z "$1" ]; then
+    ${BASE_DIR}/clean-containers.sh
+fi
+${BASE_DIR}/smoke-test.sh ${BASE_DIR}/../.git $1
