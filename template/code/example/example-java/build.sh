66ee7280d2f2e4008a6a763d5d61c8866b5d2473
ototadana <ototadana@gmail.com>
2016-10-16 08:37:15 +0900

    Fix the build failure on jenkins

--- template/code/example/example-java/build.sh
+++ template/code/example/example-java/build.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ -z "${CI_BUILD_REF}" ]; then
     CI_BUILD_REF="${GIT_COMMIT}"
-    CI_BUILD_REF_NAME="${gitlabBranch}"
+    CI_BUILD_REF_NAME="${GIT_BRANCH#*/}"
 fi
 
 BUILD_OPTS="-Dmaven.test.failure.ignore=true -Dsonar.analysis.mode=${SONAR_ANALYSIS_MODE:-publish} -Dsonar.gitlab.project_id=example/example-java -Dsonar.gitlab.commit_sha=${CI_BUILD_REF} -Dsonar.gitlab.ref_name=${CI_BUILD_REF_NAME}"
--- template/code/example/example-nodejs/build.sh
+++ template/code/example/example-nodejs/build.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ -z "${CI_BUILD_REF}" ]; then
     export CI_BUILD_REF="${GIT_COMMIT}"
-    export CI_BUILD_REF_NAME="${gitlabBranch}"
+    export CI_BUILD_REF_NAME="${GIT_BRANCH#*/}"
 fi
 
 npm install
